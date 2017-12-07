51edb9dda9bbc9d2461467105f097fb70b2b52d4
pxgamer <owzie123@gmail.com>
2017-12-07 17:55:19 +0000

    Beautify src/ directory

--- src/Plugin.php
+++ src/Plugin.php
@@ -2,17 +2,23 @@
 
 namespace MiniminPackage\ServerStats;
 
+/**
+ * Class Plugin.
+ */
 class Plugin
 {
+    /**
+     * @return object
+     */
     public static function info()
-    {        
+    {
         $object = (object)[
             'app_namespace' => '\\MiniminPackage\\ServerStats',
-            'name' => 'Server Stats',
-            'link' => 'server-stats',
-            'description' => 'A server statistics package based around system information.'
+            'name'          => 'Server Stats',
+            'link'          => 'server-stats',
+            'description'   => 'A server statistics package based around system information.',
         ];
-        
+
         return $object;
     }
 }
